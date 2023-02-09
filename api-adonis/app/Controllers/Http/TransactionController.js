0831fbe0745ad6b0ae0dd9315145a6163533adc4
Euler R. Sudbrack <euler.sudbrack@gmail.com>
2023-02-09 02:22:53 -0300

    Função de import agora aceita upload de arquivo

--- api-adonis/app/Controllers/Http/TransactionController.js
+++ api-adonis/app/Controllers/Http/TransactionController.js
@@ -1,34 +1,27 @@
 'use strict'
 const Transaction = use("App/Models/Transaction");
+const Helpers = use('Helpers')
+const fs = require('fs');
 
 class TransactionController {
     async import({request, response}) {
         try {
-            let fileContent = `12022-01-15T19:20:30-03:00CURSO DE BEM-ESTAR            0000012750JOSE CARLOS
-12021-12-03T11:46:02-03:00DOMINANDO INVESTIMENTOS       0000050000MARIA CANDIDA
-22022-01-16T14:13:54-03:00CURSO DE BEM-ESTAR            0000012750THIAGO OLIVEIRA
-32022-01-16T14:13:54-03:00CURSO DE BEM-ESTAR            0000004500THIAGO OLIVEIRA
-42022-01-16T14:13:54-03:00CURSO DE BEM-ESTAR            0000004500JOSE CARLOS
-12022-01-22T08:59:13-03:00DOMINANDO INVESTIMENTOS       0000050000MARIA CANDIDA
-12022-02-01T23:35:43-03:00DESENVOLVEDOR FULL STACK      0000155000ELIANA NOGUEIRA
-22022-02-03T17:23:37-03:00DESENVOLVEDOR FULL STACK      0000155000CARLOS BATISTA
-22022-02-03T20:51:59-03:00DESENVOLVEDOR FULL STACK      0000155000CAROLINA MACHADO
-22022-02-04T07:42:12-03:00DESENVOLVEDOR FULL STACK      0000155000CELSO DE MELO
-32022-02-03T17:23:37-03:00DESENVOLVEDOR FULL STACK      0000050000CARLOS BATISTA
-32022-02-03T20:51:59-03:00DESENVOLVEDOR FULL STACK      0000050000CAROLINA MACHADO
-32022-02-04T07:42:12-03:00DESENVOLVEDOR FULL STACK      0000050000CELSO DE MELO
-42022-02-03T17:23:37-03:00DESENVOLVEDOR FULL STACK      0000050000ELIANA NOGUEIRA
-42022-02-03T20:51:59-03:00DESENVOLVEDOR FULL STACK      0000050000ELIANA NOGUEIRA
-42022-02-04T07:42:12-03:00DESENVOLVEDOR FULL STACK      0000050000ELIANA NOGUEIRA
-12022-02-19T05:33:07-03:00DOMINANDO INVESTIMENTOS       0000050000MARIA CANDIDA
-12022-03-01T02:09:54-03:00CURSO DE BEM-ESTAR            0000012750JOSE CARLOS
-12022-03-03T09:07:35-03:00DESENVOLVEDOR FULL STACK      0000155000ELIANA NOGUEIRA
-12022-03-03T13:12:16-03:00DESENVOLVEDOR FULL STACK      0000155000ELIANA NOGUEIRA
+            const sales = request.file('sales', {
+                extnames: ['txt'],
+                size: '2mb'
+            })
 
-`
+            await sales.move(Helpers.tmpPath('uploads'), {
+                name: 'sales.txt',
+                overwrite: true
+            })
+
+            if (!sales.moved()) {
+                return sales.error()
+            }
 
             // Split file content into array and remove empty lines
-            let transactions = fileContent.split('\n')
+            let transactions = fs.readFileSync(`${Helpers.tmpPath('uploads')}/sales.txt`, 'utf8').toString().split("\n");
             transactions = transactions.filter(entry => entry.trim() != '');
             
             const convertLineToTransactionObject = (line) => {
